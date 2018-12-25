149797fffb0f30a1e16d97f40186f462fc769c4d
cnusrini <cnu.srini@hotmail.com>
2018-12-25 03:11:14 -0500

    included state variables, enum and the mapping between sku and item

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -2,8 +2,28 @@ pragma solidity ^0.5.0;
 
 contract SupplyChain {
 
-
+  // Owner is the person who deploy the smartContract
   address owner;
+  // skuCount to track the most recent sku # */
+  uint skuCount;
+
+  struct Item {
+    string name;
+    uint sku;
+    uint price;
+    address seller;
+    address buyer;
+    State state;
+
+  }
+
+  /* A public mapping that maps the SKU (a number) to an Item defined in the Struct Item.
+
+  */
+  mapping(uint => Item) public items;
+  // Possible states of an item at any given time in its supplyChain life cycle
+  enum State {ForSale, Sold, Shipped, Received}
+
 
   constructor() public {
     owner = msg.sender;
